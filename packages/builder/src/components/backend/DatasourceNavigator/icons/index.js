03731cba205388b7060c84c5053822597b56a614
Hervé de CHAVIGNY <vevedh@gmail.com>
2022-09-08 08:21:51 -0400

    update from fork

--- packages/builder/src/components/backend/DatasourceNavigator/icons/index.js
+++ packages/builder/src/components/backend/DatasourceNavigator/icons/index.js
@@ -15,12 +15,12 @@ import GoogleSheets from "./GoogleSheets.svelte"
 import Firebase from "./Firebase.svelte"
 import Redis from "./Redis.svelte"
 import Snowflake from "./Snowflake.svelte"
-<<<<<<< HEAD
+
 import SP2019 from "./SP2019.svelte"
 import ADLDAP from "./ADLDAP.svelte"
-=======
+
 import Custom from "./Custom.svelte"
->>>>>>> develop
+
 
 const ICONS = {
   BUDIBASE: Budibase,
@@ -40,10 +40,10 @@ const ICONS = {
   FIRESTORE: Firebase,
   REDIS: Redis,
   SNOWFLAKE: Snowflake,
-<<<<<<< HEAD
+
   SP2019: SP2019,
-  ADLDAP: ADLDAP
-=======
+  ADLDAP: ADLDAP,
+
   CUSTOM: Custom,
 }
 
@@ -55,5 +55,5 @@ export function getIcon(integrationType, schema) {
   } else {
     return ICONS[integrationType]
   }
->>>>>>> develop
+
 }
